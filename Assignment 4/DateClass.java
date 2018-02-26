2c556faae2ae4b271f1cbd5edd9b33d337574683
Charlton Trezevant <ct@ctis.me>
2018-02-26 18:43:24 -0500

    Finish DateClass.java

--- Assignment 4/DateClass.java
+++ Assignment 4/DateClass.java
@@ -1,94 +1,116 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class DateClass {
-  private int day, month, year, dayInYear, dateType;
+  private Calendar cal;
+  private SimpleDateFormat formatter;
   private String monthName;
-  private int[][] months = {{1, 31, 0},{2, 28, 59},{3, 31, 90},{4, 30, 120},{5, 31, 151},{6, 30, 181},{7, 31, 212},{8, 31, 243},{9, 30, 273},{10, 31, 304},{11, 30, 334},{12, 31, 365}};
-  
+
   public DateClass() {
-    this.month = 1;
-    this.day = 1;
-    this.year = 1970;
-    this.dateType = 1;
+    this.cal = new GregorianCalendar();
+    this.cal.set(Calendar.MONTH, 0);
+    this.cal.set(Calendar.DAY_OF_MONTH, 0);
+    this.cal.set(Calendar.YEAR, 1970);
   }
   
   public DateClass(int m, int d, int y) {
-    this.month = m;
-    this.day = d;
-    this.year = y;
-    this.dateType = 1;
+    this.cal = new GregorianCalendar();
+    this.cal.set(Calendar.MONTH, m - 1);
+    this.cal.set(Calendar.DAY_OF_MONTH, d);
+    this.cal.set(Calendar.YEAR, y);
   }
   
   public DateClass(String m, int d, int y) {
-    this.monthName = m;
-    this.day = d;
-    this.year = y;
-    this.dateType = 2;
+    this.cal = new GregorianCalendar();
+    this.cal.set(Calendar.MONTH, this.strToMonth(m));
+    this.cal.set(Calendar.DAY_OF_MONTH, d);
+    this.cal.set(Calendar.YEAR, y);
   }
   
   public DateClass(int d, int y) {
-    this.day = d;
-    this.year = y;
-    this.dateType = 3;
+    this.cal = new GregorianCalendar();
+    this.cal.set(Calendar.DAY_OF_YEAR, d);
+    this.cal.set(Calendar.YEAR, y);
   }
   
   public int getMonth() {
-    return this.month;
+    return this.cal.get(Calendar.MONTH);
   }
   
   public void setMonth(int m){
-    this.month = m;
+    this.cal.set(Calendar.MONTH, m);
   }
   
   public int getDay() {
-    return this.day;
+    return this.cal.get(Calendar.DAY_OF_MONTH);
   }
   
   public void setDay(int d){
-    this.day = d;
+    this.cal.set(Calendar.DAY_OF_MONTH, d);
   }
   
   public int getYear() {
-    return this.year;
+    return this.cal.get(Calendar.YEAR);
   }
   
   public void setYear(int y){
-    this.year = y;
+    this.cal.set(Calendar.YEAR, y);
+  }
+
+  public String toString() {
+    return String.format("%d/%d/%d%n", this.cal.get(Calendar.MONTH)+1, this.cal.get(Calendar.DAY_OF_MONTH), this.cal.get(Calendar.YEAR));
   }
 
   public String toMonthNameDateString() {
-    return this.toString();
+    return String.format("%s %d, %d%n", this.cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()), this.cal.get(Calendar.DAY_OF_MONTH), this.cal.get(Calendar.YEAR));
   }
   
   public String toDayDateString() {
-    return this.toString();
+    return String.format("%d %d%n", this.cal.get(Calendar.DAY_OF_YEAR), this.cal.get(Calendar.YEAR));
   }
   
-  public int domFromDoy(int day) {
-//   private int[][] months = {{1, 31, 0},{2, 28, 59},{3, 31, 90},{4, 30, 120},{5, 31, 151},{6, 30, 181},{7, 31, 212},{8, 31, 243},{9, 30, 273},{10, 31, 304},{11, 30, 334},{12, 31, 365}};
-    int candidate = 0;
-    int index = -1;
-    while(day > candidate) {
-      index++;
-      candidate += this.months[index][2];
-    }
-    
-    
-    
-  }
-  
-  public String toString() {
-    switch(this.dateType){
-      
-      case 1:
-        return String.format("%d/%d/%d%n", this.month, this.day, this.year);
-      
-      case 2:
-        return String.format("%d %d, %d%n", this.monthName, this.day, this.year);
+  public int strToMonth(String month) {
+    switch(month.toLowerCase()){
+      case "january":
+        return Calendar.JANUARY;
         
-      case 3:
-        return String.format("%.3d %d%n", this.day, this.year);
-
+      case "february":
+        return Calendar.FEBRUARY;
+      
+      case "march":
+        return Calendar.MARCH;
+        
+      case "april":
+        return Calendar.APRIL;
+        
+      case "may":
+        return Calendar.MAY;
+        
+      case "june":
+        return Calendar.JUNE;
+        
+      case "july":
+        return Calendar.JULY;
+        
+      case "august":
+        return Calendar.AUGUST;
+        
+      case "september":
+        return Calendar.SEPTEMBER;
+        
+      case "october":
+        return Calendar.OCTOBER;
+        
+      case "november":
+        return Calendar.NOVEMBER;
+        
+      case "december":
+        return Calendar.DECEMBER;
+      
       default:
-        return "";
+        return Calendar.JANUARY;
     }
   }
 }
