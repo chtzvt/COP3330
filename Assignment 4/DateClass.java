7108e5a440c6c069c5b8ca60f33f8a85f1c1d4ce
Charlton Trezevant <ct@ctis.me>
2018-02-26 18:46:16 -0500

    Add comments

--- Assignment 4/DateClass.java
+++ Assignment 4/DateClass.java
@@ -8,6 +8,9 @@ public class DateClass {
   private SimpleDateFormat formatter;
   private String monthName;
 
+  // Overloaded constructors
+  
+  // Default (no arguments - Jan 1 1970)
   public DateClass() {
     this.cal = new GregorianCalendar();
     this.cal.set(Calendar.MONTH, 0);
@@ -15,6 +18,7 @@ public class DateClass {
     this.cal.set(Calendar.YEAR, 1970);
   }
   
+  // Month, day, and year provided as integers
   public DateClass(int m, int d, int y) {
     this.cal = new GregorianCalendar();
     this.cal.set(Calendar.MONTH, m - 1);
@@ -22,6 +26,7 @@ public class DateClass {
     this.cal.set(Calendar.YEAR, y);
   }
   
+  // Month provided as String, day and year provided as integers
   public DateClass(String m, int d, int y) {
     this.cal = new GregorianCalendar();
     this.cal.set(Calendar.MONTH, this.strToMonth(m));
@@ -29,12 +34,15 @@ public class DateClass {
     this.cal.set(Calendar.YEAR, y);
   }
   
+  // day of year and year provided as integers
   public DateClass(int d, int y) {
     this.cal = new GregorianCalendar();
     this.cal.set(Calendar.DAY_OF_YEAR, d);
     this.cal.set(Calendar.YEAR, y);
   }
   
+  
+  // Setters and getters for month/day/year
   public int getMonth() {
     return this.cal.get(Calendar.MONTH);
   }
@@ -59,18 +67,22 @@ public class DateClass {
     this.cal.set(Calendar.YEAR, y);
   }
 
+  // default tostring is MM/DD/YYYY
   public String toString() {
     return String.format("%d/%d/%d%n", this.cal.get(Calendar.MONTH)+1, this.cal.get(Calendar.DAY_OF_MONTH), this.cal.get(Calendar.YEAR));
   }
 
+  // Month name plus day of month plus year
   public String toMonthNameDateString() {
     return String.format("%s %d, %d%n", this.cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()), this.cal.get(Calendar.DAY_OF_MONTH), this.cal.get(Calendar.YEAR));
   }
   
+  // Day of Year plus Year
   public String toDayDateString() {
     return String.format("%d %d%n", this.cal.get(Calendar.DAY_OF_YEAR), this.cal.get(Calendar.YEAR));
   }
   
+  // Converts lowercase strings into a Month index that we can pass to our Calendar instance
   public int strToMonth(String month) {
     switch(month.toLowerCase()){
       case "january":
